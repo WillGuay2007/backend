0c0fb46774357f90dd3215605f34fe19806a22d5
William Guay <2430063@collegebart.ca>
2025-12-05 15:19:08 -0500

    e

--- server.js
+++ server.js
@@ -27,6 +27,22 @@ app.post("/signup", (req, res) => {
         return res.json.data
     })
 })
+app.post("/signin", (req, res) => {
+    const sql = `SELECT * FROM users WHERE email = ? AND password = ?`
+    const values = [
+        req.body.email,
+        req.body.password
+    ]
+        
+    db.query(sql, values, (err, data) => {
+        if(err) {
+            return res.json(err)
+        } else {
+             return res.json(data.length > 0)
+        }
+       
+    })
+})
 
 app.listen(8081, () =>
     console.log("Coucou je suis le server..")
